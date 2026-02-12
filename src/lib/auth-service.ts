754dc06ce47ffd82db8cf37e6652ef27af523374
johnraivenolazo <johnraivenolazo@gmail.com>
2026-02-12 14:26:25 +0800

    fix: authentication to force google account selection

--- src/lib/auth-service.ts
+++ src/lib/auth-service.ts
@@ -1,11 +1,18 @@
 import { Auth, signInWithPopup, GoogleAuthProvider, signOut, User } from 'firebase/auth';
 
 const googleProvider = new GoogleAuthProvider();
-googleProvider.setCustomParameters({ hd: 'neu.edu.ph' });
+googleProvider.setCustomParameters({
+    hd: 'neu.edu.ph',
+    prompt: 'select_account'
+});
 
 export async function signInWithGoogle(auth: Auth, loginHint?: string): Promise<User> {
     if (loginHint) {
-        googleProvider.setCustomParameters({ hd: 'neu.edu.ph', login_hint: loginHint });
+        googleProvider.setCustomParameters({
+            hd: 'neu.edu.ph',
+            login_hint: loginHint,
+            prompt: 'select_account'
+        });
     }
 
     const result = await signInWithPopup(auth, googleProvider);
